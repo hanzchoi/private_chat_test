5513b378f05f9c08f6012a1470ebc750228c7eb4
personabe1984 <personabe1984@gmail.com>
2016-03-19 12:39:31 -0400

    first commit

---
+++ app/src/main/java/personabe1984/c4q/nyc/chattest/MainActivity.java
@@ -0,0 +1,13 @@
+package personabe1984.c4q.nyc.chattest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/personabe1984/c4q/nyc/chattest/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package personabe1984.c4q.nyc.chattest;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
